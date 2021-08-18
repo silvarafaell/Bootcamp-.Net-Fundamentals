eae5603bd697dbc6f897b92cc46b9714a4fe85c5
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-08-18 19:24:39 -0300

    desafios

---
+++ Introdução a Programação com C#/Desafio/Desafio/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desafio2
+{
+    class Class1
+    {
+
+    }
+}
---
+++ Introdução a Programação com C#/Desafio2/Desafio2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Desafio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quilometros = Int32.Parse(Console.ReadLine());
+            int minutos = quilometros * 2; 
+            Console.WriteLine(minutos + " minutos");
+        }
+    }
+}
