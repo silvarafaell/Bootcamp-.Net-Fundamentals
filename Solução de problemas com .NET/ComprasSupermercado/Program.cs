902b291c3ec24b66c2f4dcb2116579c87dbbb198
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-08-24 10:29:04 -0300

    desafioSupermercado

---
+++ Solução de problemas com .NET/ComprasSupermercado/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ComprasSupermercado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var totalDeCasosDeTeste = int.Parse(Console.ReadLine());
+        
+            for (int i = 0; i < totalDeCasosDeTeste; i++) {
+                List<string> listDeCompras = new List<String>(Console.ReadLine().Split(' '));
+                List<string> listaSemDuplicados = listDeCompras.Distinct().ToList();
+
+                listaSemDuplicados.Sort();
+            
+                foreach (string item in listaSemDuplicados) {
+                    Console.Write($"{item} ");
+                }
+                
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
